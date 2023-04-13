597d982945c02d63f8373681ba71b15a331ab310
Mark Romanov <oxxo8898@gmail.com>
2023-04-13 16:23:34 +0200

    feat: add carbon dating fun

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,10 +17,26 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {;
+  const ln2 = 0.693;
+
+  if (typeof sampleActivity !== 'string' || isNaN(parseFloat(sampleActivity))) {
+    return false;
+  }
+
+  const activity = parseFloat(sampleActivity);
+
+  if (activity <= 0 || activity >= MODERN_ACTIVITY) {
+    return false;
+  }
+
+  const age = Math.ceil((Math.log(MODERN_ACTIVITY / activity) / ln2) * HALF_LIFE_PERIOD);
+
+  return age;
 }
+
+console.log(dateSample('1')); 
+console.log(dateSample('WOOT!')); 
 
 module.exports = {
   dateSample
